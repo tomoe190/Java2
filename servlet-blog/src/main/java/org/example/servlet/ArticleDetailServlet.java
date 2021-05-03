734b39e598c9a56f17201dd6e4817a51065989b5
tomoe <593575498@qq.com>
2021-05-03 20:10:00 +0800

    servlet_blog

--- servlet-blog/src/main/java/org/example/servlet/ArticleDetailServlet.java
+++ servlet-blog/src/main/java/org/example/servlet/ArticleDetailServlet.java
@@ -1,8 +1,17 @@
 package org.example.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet("/")
 public class ArticleDetailServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
 }
