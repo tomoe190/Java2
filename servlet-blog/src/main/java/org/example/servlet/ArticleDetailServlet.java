5e04ec46ed01cc6ffe815e3349e9079bb36ba7f7
tomoe <593575498@qq.com>
2021-05-03 23:56:46 +0800

    servlet_blog

--- servlet-blog/src/main/java/org/example/servlet/ArticleDetailServlet.java
+++ servlet-blog/src/main/java/org/example/servlet/ArticleDetailServlet.java
@@ -14,5 +14,6 @@ public class ArticleDetailServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
+        resp.setContentType();
     }
 }
